f87101f8695f2e93c6b36bf3cbd3bd56ca9b89f4
noam jacobson <noam.jacobson@protonmail.com>
2018-11-09 21:30:09 -0500

    Added var songs, artists & albums. Added Filter func to filter songs. Added types for song, artist & album. added rootQuery and resolving songs, album & artist.

--- main.go
+++ main.go
@@ -3,39 +3,195 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	"github.com/graphql-go/graphql"
 )
 
 type Album struct {
-	ID		string `json:"id:omitempty"`
-	Artist	string `json:"artist"`
-	Title	string `json:"title"`
-	Year	string `json:"year"`
-	Gender	string `json:"gender"`
-	Type	string `json:"type"`
+	ID     string `json:"id:omitempty"`
+	Artist string `json:"artist"`
+	Title  string `json:"title"`
+	Year   string `json:"year"`
+	Genre  string `json:"genre"`
+	Type   string `json:"type"`
 }
 
 type Artist struct {
-	ID		string `json:"id:omitempty"`
-	Name	string `json:"name"`
-	Type	string `json:"type"`
+	ID   string `json:"id:omitempty"`
+	Name string `json:"name"`
+	Type string `json:"type"`
 }
 
 type Song struct {
-	ID			string `json:"id:omitempty"`
-	Album		string `json:"album"`
-	Title		string `json:"title"`
-	Duration	string `json:"duration"`
-	Type		string `json:"type"`
+	ID       string `json:"id:omitempty"`
+	Album    string `json:"album"`
+	Title    string `json:"title"`
+	Duration string `json:"duration"`
+	Type     string `json:"type"`
+}
+
+var albums []Album = []Album{
+	Album{
+		ID:     "ts-dark-as-night",
+		Artist: "1",
+		Title:  "Dark As Night",
+		Year:   "2008",
+		Type:   "album",
+	},
+}
+
+var artists []Artist = []Artist{
+	Artist{
+		ID:   "1",
+		Name: "Nahko and Medicine for the People",
+		Type: "artist",
+	},
+}
+
+var songs []Song = []Song{
+	Song{
+		ID:       "1",
+		Album:    "ts-dark-as-night",
+		Title:    "Buddying Trees",
+		Duration: "06:24",
+		Type:     "song",
+	},
+	Song{
+		ID:       "2",
+		Album:    "ts-dark-as-night",
+		Title:    "Aloha Ke Akua",
+		Duration: "05:56",
+		Type:     "song",
+	},
+}
+
+func Filter(songs []Song, f func(Song) bool) []Song {
+	vsf := make([]Song, 0)
+	for _, v := range songs {
+		if f(v) {
+			vsf = append(vsf, v)
+		}
+	}
+	return vsf
 }
 
 func main() {
-	schema, _ := graphql.NewSchema(graphql.SchemaConfig{})
-	http.HandleFunc("graphql", func(w http.ResponseWriter, r *http.Request) {
+	songType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Song",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"album": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"duraction": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+	artistType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Artist",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+	albumType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Album",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"artist": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"year": &graphql.Field{
+				Type: graphql.String,
+			},
+			"genre": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"songs": &graphql.Field{
+				Type: graphql.NewList(songType),
+				Args: graphql.FieldConfigArgument{
+					"album": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					album := params.Args["album"].(string)
+					filtered := Filter(songs, func(v Song) bool {
+						return strings.Contains(v.Album, album)
+					})
+					return filtered, nil
+				},
+			},
+			"album": &graphql.Field{
+				Type: albumType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					id := params.Args["id"].(string)
+					for _, album := range albums {
+						if album.ID == id {
+							return album, nil
+						}
+					}
+					return nil, nil
+				},
+			},
+			"artist": &graphql.Field{
+				Type: artistType,
+				Args: graphql.FieldConfigArgument{
+					"name": &graphql.ArgumentConfig{
+						Type: graphql.NewNonNull(graphql.String),
+					},
+				},
+				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+					name := params.Args["name"].(string)
+					for _, artist := range artists {
+						if artist.Name == name {
+							return artist, nil
+						}
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
 		result := graphql.Do(graphql.Params{
-			Schema:			schema,
-			RequestString:	r.URL.Query().Get("query"),
+			Schema:        schema,
+			RequestString: r.URL.Query().Get("query"),
 		})
 		json.NewEncoder(w).Encode(result)
 	})
