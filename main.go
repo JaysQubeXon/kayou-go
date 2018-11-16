2dbea644f81b533c9d7aed95b7c1e1b099f42b14
noam jacobson <noam.jacobson@protonmail.com>
2018-11-15 21:12:51 -0500

    changed comment type to appease linter

--- main.go
+++ main.go
@@ -68,13 +68,11 @@ var songs = []Song{
 	},
 }
 
-/**
- * Filter receives songs as first param
- * and a func as second param, that checks if
- * value is a song in the Song array. If it is,
- * it appends it to a new array of type Song
- * and returns it.
- */
+// Filter receives songs as first param
+// and a func as second param, that checks if
+// value is a song in the Song array. If it is,
+// it appends it to a new array of type Song
+// and returns it.
 func Filter(songs []Song, f func(Song) bool) []Song {
 	vsf := make([]Song, 0)
 	for _, v := range songs {
