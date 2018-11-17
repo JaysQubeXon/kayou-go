5957d273029098c459c7a837c8561bddd7fb4fe4
noam jacobson <noam.jacobson@protonmail.com>
2018-11-17 09:41:25 -0500

    moved data types to own file

--- main.go
+++ main.go
@@ -10,60 +10,7 @@ import (
 )
 
 func main() {
-	songType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Song",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"album": &graphql.Field{
-				Type: graphql.String,
-			},
-			"title": &graphql.Field{
-				Type: graphql.String,
-			},
-			"duration": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	})
-	artistType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Artist",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"type": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	})
-	albumType := graphql.NewObject(graphql.ObjectConfig{
-		Name: "Album",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"artist": &graphql.Field{
-				Type: graphql.String,
-			},
-			"title": &graphql.Field{
-				Type: graphql.String,
-			},
-			"year": &graphql.Field{
-				Type: graphql.String,
-			},
-			"genre": &graphql.Field{
-				Type: graphql.String,
-			},
-			"type": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	})
+	albumType, artistType, songType := dataTypes()
 	rootQuery := graphql.NewObject(graphql.ObjectConfig{
 		Name: "Query",
 		Fields: graphql.Fields{
---
+++ schema.go
@@ -0,0 +1,62 @@
+package main
+
+import "github.com/graphql-go/graphql"
+
+func dataTypes() (*graphql.Object, *graphql.Object, *graphql.Object) {
+	songType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Song",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"album": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"duration": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+	artistType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Artist",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+	albumType := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Album",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"artist": &graphql.Field{
+				Type: graphql.String,
+			},
+			"title": &graphql.Field{
+				Type: graphql.String,
+			},
+			"year": &graphql.Field{
+				Type: graphql.String,
+			},
+			"genre": &graphql.Field{
+				Type: graphql.String,
+			},
+			"type": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	})
+
+	return albumType, artistType, songType
+}
