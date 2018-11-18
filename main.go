5c941eda06ca9d5bcef1c447c229b97a860b0b86
noam jacobson <noam.jacobson@protonmail.com>
2018-11-18 18:52:36 -0500

    curl query successful. start of implementation to gophers graphql-go package

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
-	"github.com/graphql-go/graphql"
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
 )
 
+type query struct{}
+
+func (_ *query) Hello() string { return "Hello, world!\n" }
+
 func main() {
-	rootQuery, _ := RootQuery(dataTypes())
-	rootMutation, _ := RootMutation(dataTypes())
-	schema, _ := graphql.NewSchema(graphql.SchemaConfig{
-		Query:    rootQuery,
-		Mutation: rootMutation,
-	})
-	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-		result := graphql.Do(graphql.Params{
-			Schema:        schema,
-			RequestString: r.URL.Query().Get("query"),
-		})
-		json.NewEncoder(w).Encode(result)
-	})
+	s := `
+		schema {
+			query: Query
+		}
+		type Query {
+			hello: String!
+		}
+	`
+	schema := graphql.MustParseSchema(s, &query{})
+	http.Handle("/query", &relay.Handler{Schema: schema})
 	fmt.Printf("server listening on port 8080\n")
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
