53fe50b06b12312b1fe01f256e2c3c4116abc8d4
noam jacobson <noam.jacobson@protonmail.com>
2018-11-19 18:35:43 -0500

    refactored queries to fit new package specs. unfinished - need to resolve some issues.

--- query.go
+++ query.go
@@ -3,85 +3,40 @@ package main
 import (
 	"strings"
 
-	"github.com/graphql-go/graphql"
+	graphql "github.com/graph-gophers/graphql-go"
 )
 
-// "Root query"
-func RootQuery(albumType graphql.Type, artistType graphql.Type, songType graphql.Type) (*graphql.Object, error) {
-	return graphql.NewObject(graphql.ObjectConfig{
-		Name:        "Query",
-		Description: "Root query object",
-		Fields: graphql.Fields{
-			"songs": &graphql.Field{
-				Type:        graphql.NewList(songType),
-				Description: "Get all songs in an album",
-				Args: graphql.FieldConfigArgument{
-					"album": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					album := params.Args["album"].(string)
-					filtered := Filter(songs, func(v Song) bool {
-						return strings.Contains(v.Album, album)
-					})
-					return filtered, nil
-				},
-			},
-			"album": &graphql.Field{
-				Type:        albumType,
-				Description: "Get album by ID",
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					id := params.Args["id"].(string)
-					for _, album := range albums {
-						if album.ID == id {
-							return album, nil
-						}
-					}
-					return nil, nil
-				},
-			},
-			"artist": &graphql.Field{
-				Type:        artistType,
-				Description: "Get artist by name",
-				Args: graphql.FieldConfigArgument{
-					"name": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					name := params.Args["name"].(string)
-					for _, artist := range artists {
-						if artist.Name == name {
-							return artist, nil
-						}
-					}
-					return nil, nil
-				},
-			},
-			"genre": &graphql.Field{
-				Type:        albumType,
-				Description: "Get albums by genre kind",
-				Args: graphql.FieldConfigArgument{
-					"kind": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: func(params graphql.ResolveParams) (interface{}, error) {
-					kind := params.Args["kind"].(string)
-					for _, album := range albums {
-						if album.Genre == kind {
-							return album, nil
-						}
-					}
-					return nil, nil
-				},
-			},
-		},
-	}), nil
+func (_ *Resolver) Songs(album string) []*Song {
+	var filtered []*Song
+	filtered = Filter(*songs, func(v Song) bool {
+		return strings.Contains(v.Album, album)
+	})
+	return filtered
+}
+
+func (_ *Resolver) Album(id graphql.ID) interface{} {
+	for _, album := range albums {
+		if album.ID == id {
+			return album
+		}
+	}
+	return nil
+}
+
+func (_ *Resolver) Artist(name string) interface{} {
+	for _, artist := range artists {
+		if artist.Name == name {
+			return artist
+		}
+	}
+	return nil
+}
+
+func (_ *Resolver) Genre(kind string) interface{} {
+	for _, album := range albums {
+		if album.Genre == kind {
+			return album
+		}
+	}
+	return nil
 }
