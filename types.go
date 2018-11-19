a40f82ea82feb228a2ce58f6fe7f78579ec6f9d6
noam jacobson <noam.jacobson@protonmail.com>
2018-11-19 18:34:52 -0500

    refactored type structs and added some resolvers.

--- types.go
+++ types.go
@@ -1,27 +1,96 @@
 package main
 
+import graphql "github.com/graph-gophers/graphql-go"
+
 // "Album"
 type Album struct {
-	ID     string `json:"id:omitempty"`
-	Artist string `json:"artist"`
-	Title  string `json:"title"`
-	Year   string `json:"year"`
-	Genre  string `json:"genre"`
-	Type   string `json:"type"`
+	ID     graphql.ID
+	Artist string
+	Title  string
+	Year   string
+	Genre  string
+	Type   string
+}
+
+type AlbumResolver struct {
+	album *Album
+}
+
+func (resolve *AlbumResolver) ID() graphql.ID {
+	return resolve.album.ID
+}
+
+func (resolve *AlbumResolver) Artist() string {
+	return resolve.album.Artist
+}
+
+func (resolve *AlbumResolver) Title() string {
+	return resolve.album.Title
+}
+
+func (resolve *AlbumResolver) Year() string {
+	return resolve.album.Year
+}
+
+func (resolve *AlbumResolver) Genre() string {
+	return resolve.album.Genre
+}
+
+func (resolve *AlbumResolver) Type() string {
+	return resolve.album.Type
 }
 
 // "Artist"
 type Artist struct {
-	ID   string `json:"id:omitempty"`
-	Name string `json:"name"`
-	Type string `json:"type"`
+	ID   graphql.ID
+	Name string
+	Type string
 }
 
-// "Song"
+type ArtistResolver struct {
+	artist *Artist
+}
+
+func (resolve *ArtistResolver) ID() graphql.ID {
+	return resolve.artist.ID
+}
+
+func (resolve *ArtistResolver) Name() string {
+	return resolve.artist.Name
+}
+
+func (resolve *ArtistResolver) Type() string {
+	return resolve.artist.Type
+}
+
 type Song struct {
-	ID       string `json:"id:omitempty"`
-	Album    string `json:"album"`
-	Title    string `json:"title"`
-	Duration string `json:"duration"`
-	Type     string `json:"type"`
+	ID       graphql.ID
+	Album    string
+	Title    string
+	Duration string
+	Type     string
+}
+
+type SongResolver struct {
+	song *Song
+}
+
+func (resolve *SongResolver) ID() graphql.ID {
+	return resolve.song.ID
+}
+
+func (resolve *SongResolver) Album() string {
+	return resolve.song.Album
+}
+
+func (resolve *SongResolver) Title() string {
+	return resolve.song.Title
+}
+
+func (resolve *SongResolver) Duration() string {
+	return resolve.song.Duration
+}
+
+func (resolve *SongResolver) Type() string {
+	return resolve.song.Type
 }
